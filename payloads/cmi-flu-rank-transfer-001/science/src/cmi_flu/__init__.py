a69696a44f82d0306b9fb930de2d669842ef8266
renta0426 <71638068+renta0426@users.noreply.github.com>
2026-09-04 13:56:11 +0900

    cleanup: remove unused private science package copy

--- payloads/cmi-flu-rank-transfer-001/science/src/cmi_flu/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CMI-Flu Part 1 baseline package."""
-
-from .contracts import PUBLIC_LEADERBOARD_TASKS, TASK_COLUMNS
-
-__all__ = ["PUBLIC_LEADERBOARD_TASKS", "TASK_COLUMNS"]
-__version__ = "0.1.0"
